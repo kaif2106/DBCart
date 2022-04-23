20385410af3f11693a58b6edd00245a3fbcaac97
Mohammed Kaif <mohammedkaif2106@gmail.com>
2022-04-24 01:41:40 +0530

    temp

--- app.py
+++ app.py
@@ -6,20 +6,29 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_USER']="root"
-app.config['MYSQL_PASSWORD']="kushiluv25"
+app.config['MYSQL_PASSWORD']="password"
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_DB']="ecommerce"
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
 
 
 mysql = MySQL(app)
 
 @app.route("/")
-def hello_world():
+def login_page():
+    print("jh")
     cur = mysql.connection.cursor()
-    cur.execute("insert into user (username, _type, passwd) values ('kaif', 'C', 'sjxli9SQ')")
-    mysql.connection.commit()
+    cur.execute("select * from user")
+    results = cur.fetchall()
     cur.close()
-    return render_template('homepage.html')
+    print(results)
+    return render_template('homepage.html', item=results)
+
+@app.route("/wtv")
+def hello():
+    return render_template('new.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
