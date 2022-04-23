36287398a7d3e10c43e4dc66a3cd08df6cb31a98
Mohammed Kaif <mohammedkaif2106@gmail.com>
2022-04-23 20:08:49 +0530

    first_commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    return render_template('homepage.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
