dc9d3b04a4230d36566d8bc5f41e97563e99f7e4
kushiluv <kushiluvjangu@gmail.com>
2022-04-24 17:53:08 +0530

    buttondone

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_USER']="root"
-app.config['MYSQL_PASSWORD']="password"
+app.config['MYSQL_PASSWORD']="kushiluv25"
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_DB']="ecommerce"
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
